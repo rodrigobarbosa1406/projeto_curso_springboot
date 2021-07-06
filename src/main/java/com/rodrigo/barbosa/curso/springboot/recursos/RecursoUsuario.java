76354cbd79328b0dacee22b777c3e0837547bb62
rodrigobarbosa1406 <rodrigobarbosanb@hotmail.com>
2021-07-06 18:59:34 -0300

    User delete

--- src/main/java/com/rodrigo/barbosa/curso/springboot/recursos/RecursoUsuario.java
+++ src/main/java/com/rodrigo/barbosa/curso/springboot/recursos/RecursoUsuario.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +41,11 @@ public class RecursoUsuario {
 		usuario = servicoUsuario.inserir(usuario);
 		URI uriUsuario = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuario.getId()).toUri();
 		return ResponseEntity.created(uriUsuario).body(usuario);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Long id){
+		servicoUsuario.excluir(id);
+		return ResponseEntity.noContent().build();
 	}
 }
